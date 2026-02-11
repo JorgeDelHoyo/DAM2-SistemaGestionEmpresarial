7541b3e61b1a4666b3448900fb253ea886db0c8f
Jorge del Hoyo <delhoyoballestin23@iespabloserrano.com>
2026-02-11 19:34:37 +0100

    Repaso 2ªEv

---
+++ 2Ev/RepasoODOOExamen/PRUEBA_1/addons/prueba1/__manifest__.py
@@ -0,0 +1,33 @@
+{
+    'name': "prueba1",
+
+    'summary': "Prueba1 module para Odoo19",
+
+    'description': """
+    Modulo de prueba para el examen de SGE
+    """,
+
+    'author': "Jorge del Hoyo",
+    'website': "https://www.youtube.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
+
---
+++ 2Ev/RepasoODOOExamen/PRUEBA_1/addons/prueba1/models/models.py
@@ -0,0 +1,62 @@
+from odoo import _, api, fields, models
+
+# ---------------------------------------------------------
+# 1. HERENCIA PROTOTIPO (Class Inheritance)
+# Escenario: Persona -> Hijo
+# ---------------------------------------------------------
+
+class Persona(models.Model):
+    _name = 'prueba1.persona'
+    _description = 'Modelo base Persona'
+
+    name = fields.Char(string='Nombre Completo', required=True)
+    fecha_nacimiento = fields.Date(string='Fecha de Nacimiento')
+    direccion = fields.Text(string='Direccion')
+
+class Hijo(models.Model):
+    _name = 'prueba1.hijo'
+    _inherit = 'prueba1.persona' # <-- HERENCIA PROTOTIPO (Copia campos de Persona)
+    _description = 'Modelo Hijo (Alumno)'
+
+    # Campos del Hijo
+    colegio = fields.Char(string='Colegio')
+    curso = fields.Selection([
+        ('primaria', 'Primaria'),
+        ('secundaria','Secundaria')
+    ], string='Curso')
+
+    #RELACION MANY2ONE: Muchos hijos pertenecen a una empresa
+    empresa_id = fields.Many2one('prueba1.empresa', string='Empresa de Prácticas')
+
+# ---------------------------------------------------------
+# 2. HERENCIA MÚLTIPLE (Mixins)
+# Escenario: Negocio + Estructura -> Empresa
+# ---------------------------------------------------------
+
+class Negocio(models.Model):
+    _name = 'prueba1.negocio'
+    _description = 'Mixin de Negocio'
+
+    rubro = fields.Char(string='Rubro/Sector', help='Ej: Tecnológico, Alimentación')
+    cif = fields.Char(string='cif')
+
+class Estructura(models.Model):
+    _name = 'prueba1.estructura'
+    _description = 'Mixin de Estructura Física'
+
+    ubicacion = fields.Char(string='Ubicacion de la Sede')
+    metros_cuadrados = fields.Float(string='Metros Cuadrados')
+
+class Empresa(models.Model):
+    _name = 'prueba1.empresa'
+    _inherit = ['prueba1.negocio', 'prueba1.estructura'] # <-- HERENCIA MÚLTIPLE
+    _description = 'Modelo empresa'
+
+    name = fields.Char(string='Nombre de la empresa', required=True)
+
+    # RELACIÓN ONE2MANY: Una empresa tiene muchos alumnos (hijos) asignados
+    # Es la inversa del campo 'empresa_id' en el modelo 'prueba1.hijo'
+    hijo_ids = fields.One2many('prueba1.hijo', 'empresa_id', string='Alumnos Asignados')
+    
+    
+    
